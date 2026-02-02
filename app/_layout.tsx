ed55bf8dfe06bb00837e2aab4292efffd44f114f
Youngjae Kim <gomgom0324@gmail.com>
2026-02-02 22:00:23 +0900

    feat: configure React Query client

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import { TamaguiProvider } from '@tamagui/core'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import config from '../tamagui.config'
+import { queryClient } from '../src/services/queryClient'
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -11,8 +13,10 @@ export default function RootLayout() {
   if (!loaded) return null
 
   return (
-    <TamaguiProvider config={config}>
-      <Stack />
-    </TamaguiProvider>
+    <QueryClientProvider client={queryClient}>
+      <TamaguiProvider config={config}>
+        <Stack />
+      </TamaguiProvider>
+    </QueryClientProvider>
   )
 }
