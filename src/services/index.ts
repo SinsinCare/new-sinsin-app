63b299fbf5e7163b9de7e140853235e02f638e26
Youngjae Kim <gomgom0324@gmail.com>
2026-02-02 22:15:08 +0900

    feat: implement AI service for food analysis and chat

---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+export { authService } from './authService'
+export { firestoreService } from './firestoreService'
+export { aiService } from './aiService'
+export { queryClient } from './queryClient'
