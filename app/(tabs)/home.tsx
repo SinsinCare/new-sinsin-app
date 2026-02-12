0435a6a5267159114771c0eac85d4ba4dd5d55bf
현지현 <qwere4585@naver.com>
2026-02-12 14:07:43 +0900

    refactor: consolidate horizontal padding to home container

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -51,6 +51,6 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     justifyContent: "flex-start",
-    paddingHorizontal: 16,
+    paddingHorizontal: 25,
   },
 })
